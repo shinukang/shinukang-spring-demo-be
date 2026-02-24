a0678849084562a2484b767d45bb7bd13cc9f40d
shinukang <shinu9643@gmail.com>
2026-02-24 16:38:12 +0900

    [feat] 보안 설정 클래스 추가

---
+++ src/main/java/org/example/demo/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.example.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                (auth) -> auth
+                        .requestMatchers("/user/*").permitAll()
+                        .requestMatchers("/post/*").authenticated()
+        );
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+}
